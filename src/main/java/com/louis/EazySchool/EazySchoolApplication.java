8dfa8c68c27019dab869a7de97f3e95bcee372cc
LouisLu00 <lhy2002m@gmail.com>
2024-06-23 16:28:56 +0800

    Add login page and Spring security layer

--- src/main/java/com/louis/EazySchool/EazySchoolApplication.java
+++ src/main/java/com/louis/EazySchool/EazySchoolApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EazySchoolApplication {
+public class  EazySchoolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EazySchoolApplication.class, args);
